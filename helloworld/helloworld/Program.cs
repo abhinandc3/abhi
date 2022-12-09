208dc692fbac8b173641fd5880c3116b748d8e8e
C <abhinand.c@rinmac2884.intra.rakuten.co.jp>
2022-12-09 14:45:32 +0530

    first commit

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace varibles
+{
+
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var number = 112;
+        var num = 12;
+            Console.WriteLine((double)number + (double)num);
+
+            string k = "12345";
+            int i = Convert.ToInt32(k);
+            int j = int.Parse(k);
+            Console.WriteLine(i );
+            Console.WriteLine(j);
+            try
+            {
+                string n = "true";
+                bool m = Convert.ToBoolean(n);
+                Console.WriteLine(n);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+             
+    }
+}
+}
